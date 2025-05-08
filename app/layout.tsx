29b64b0eb9278e7feacb343511f4882e11eba296
DeftLuke <lukedean750@gmail.com>
2025-05-09 05:46:10 +0700

    y

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+
+import {Lusitana} from 'next/font/google';
+
+const lusitana = Lusitana({ weight: ['400', '700'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={lusitana.className}>{children}</body>
+    </html>
+  );
+}
